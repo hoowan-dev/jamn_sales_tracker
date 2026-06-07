10432a1522c9b3900061d62ffefd58b3d6c6d1f3
hoowan-dev <becerrajuan007@gmail.com>
2026-06-07 13:13:23 -0700

    Added daily sync function

---
+++ run_tracker.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d c:\dev\jamn_sales_tracker
+:: If you use a virtual environment, uncomment the next line and adjust the path
+:: call venv\Scripts\activate
+python jamn_sales_tracker.py -d 1 -gs True
